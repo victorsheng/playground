a7c8415ad903957b1657343d29c27551bece4808
victor <920443721@qq.com>
2018-03-19 15:24:37 +0800

    move

--- src/test/java/com/example/demo/reflect/ReflectReturnType.java
+++ src/test/java/com/example/demo/reflect/ReflectReturnType.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.reflect;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
--- src/test/java/com/example/demo/reflect/TestInstanceCodeBlock.java
+++ src/test/java/com/example/demo/reflect/TestInstanceCodeBlock.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.reflect;
 
 import java.util.ArrayList;
 import java.util.List;
