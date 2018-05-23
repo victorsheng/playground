2fb305e1c5f16120c24d9678d0ff0e4d63d0c481
victor <920443721@qq.com>
2018-05-23 22:51:07 +0800

    nio read file

---
+++ src/test/java/com/example/demo/nio/FileRead.java
@@ -0,0 +1,32 @@
+package com.example.demo.nio;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileRead {
+
+  public static void main(String[] args) throws IOException {
+    RandomAccessFile aFile = new RandomAccessFile("/Users/victor/tmp/qingshu.txt", "rw");
+    FileChannel inChannel = aFile.getChannel();
+
+//create buffer with capacity of 48 bytes
+    ByteBuffer buf = ByteBuffer.allocate(48);
+
+    int bytesRead = inChannel.read(buf); //read into buffer.
+    while (bytesRead != -1) {
+
+      buf.flip();  //make buffer ready for read
+
+      while (buf.hasRemaining()) {
+        System.out.print((char) buf.get()); // read 1 byte at a time
+      }
+
+      buf.clear(); //make buffer ready for writing
+      bytesRead = inChannel.read(buf);
+    }
+    aFile.close();
+  }
+
+}
