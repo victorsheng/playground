c55d1d92e8ff7c05ab019383ff92827f164720f9
victor <920443721@qq.com>
2018-10-23 16:57:35 +0800

    注释掉 暂时无法执行的代码

--- project-old/src/test/java/com/example/demo/proxy/jdk/ProxyGeneratorUtils.java
+++ project-old/src/test/java/com/example/demo/proxy/jdk/ProxyGeneratorUtils.java
@@ -1,41 +1,41 @@
-package com.example.demo.proxy.jdk;
-
-import com.example.demo.proxy.HelloWorldImpl;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import sun.misc.ProxyGenerator;
-
-public class ProxyGeneratorUtils {
-
-  /**
-   * 把代理类的字节码写到硬盘上
-   *
-   * @param path 保存路径
-   */
-  public static void writeProxyClassToHardDisk(String path) {
-    // 第一种方法，这种方式在刚才分析ProxyGenerator时已经知道了
-    // System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", true);
-
-    // 第二种方法
-
-    // 获取代理类的字节码
-    byte[] classFile = ProxyGenerator
-        .generateProxyClass("$Proxy11", HelloWorldImpl.class.getInterfaces());
-
-    FileOutputStream out = null;
-
-    try {
-      out = new FileOutputStream(path);
-      out.write(classFile);
-      out.flush();
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        out.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-}
+//package com.example.demo.proxy.jdk;
+//
+//import com.example.demo.proxy.HelloWorldImpl;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import sun.misc.ProxyGenerator;
+//
+//public class ProxyGeneratorUtils {
+//
+//  /**
+//   * 把代理类的字节码写到硬盘上
+//   *
+//   * @param path 保存路径
+//   */
+//  public static void writeProxyClassToHardDisk(String path) {
+//    // 第一种方法，这种方式在刚才分析ProxyGenerator时已经知道了
+//    // System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", true);
+//
+//    // 第二种方法
+//
+//    // 获取代理类的字节码
+//    byte[] classFile = ProxyGenerator
+//        .generateProxyClass("$Proxy11", HelloWorldImpl.class.getInterfaces());
+//
+//    FileOutputStream out = null;
+//
+//    try {
+//      out = new FileOutputStream(path);
+//      out.write(classFile);
+//      out.flush();
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//    } finally {
+//      try {
+//        out.close();
+//      } catch (IOException e) {
+//        e.printStackTrace();
+//      }
+//    }
+//  }
+//}
