aa5e6db2b295315d99f4ef17b1cc21505f9ee2d9
victor <920443721@qq.com>
2018-11-06 14:19:14 +0800

    modpow

---
+++ security/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class Test {
+
+  public static void main(String[] args) {
+    BigInteger integer = BigInteger.valueOf(65l);
+    BigInteger integer1 = integer.modPow(BigInteger.valueOf(17), BigInteger.valueOf(3233));
+    System.out.println(integer1);
+
+  }
+
+}
