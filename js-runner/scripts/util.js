852a1fac195f93e8c1fee1ad5f3d34a98130a850
victor <920443721@qq.com>
2018-11-23 11:05:40 +0800

    js-runner

---
+++ js-runner/scripts/util.js
@@ -0,0 +1,10 @@
+function priceTransfer(param) {
+  // console.log(param)
+  return parseInt(param);
+}
+
+function fun2(map) {
+  // console.log(map)
+  map["3"] = "3";
+  return map;
+}
---
+++ js-runner/src/JSDynamic.java
@@ -0,0 +1,62 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class JSDynamic {
+
+  public static ScriptEngine initEngine() {
+    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    return engine;
+  }
+
+  private static FileReader getScriptFile() throws FileNotFoundException {
+    Path file = Paths.get("js-runner/scripts", "util.js").toAbsolutePath();
+    return new FileReader(file.toFile());
+  }
+
+  private static void exeFile(FileReader fr, ScriptEngine engine) {
+    try {
+      engine.eval(fr);
+    } catch (ScriptException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void invokeFunByJavaObject(Object price)
+      throws FileNotFoundException, NoSuchMethodException, ScriptException {
+    ScriptEngine engine = initEngine();
+    exeFile(getScriptFile(), engine);
+    Invocable invocable = (Invocable) engine;
+
+    Object result = invocable.invokeFunction("priceTransfer", price);
+    System.out.println(result);
+    System.out.println(result.getClass());
+  }
+
+  public static void invokeFunByJavaObject1(Object obj)
+      throws FileNotFoundException, NoSuchMethodException, ScriptException {
+    ScriptEngine engine = initEngine();
+    exeFile(getScriptFile(), engine);
+    Invocable invocable = (Invocable) engine;
+    System.out.println(System.identityHashCode(obj));
+    Object result = invocable.invokeFunction("fun2", obj);
+    System.out.println(System.identityHashCode(result));
+    System.out.println(result.getClass());
+    System.out.println(result.hashCode());
+  }
+
+  public static void main(String[] args) throws Exception {
+    invokeFunByJavaObject("762");
+    HashMap<Object, Object> param = new HashMap<>();
+    param.put("1", "1");
+    param.put("2", "2");
+    invokeFunByJavaObject1(param);
+  }
+
+}
